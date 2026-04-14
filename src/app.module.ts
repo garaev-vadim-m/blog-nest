52b16647607bce3ac064165fc0a8114c7597f253
garaev-vadim-m <garaev.v.m@icloud.com>
2026-04-14 16:00:29 +0300

    Init docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,13 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
+      host: 'db',
       port: 3306,
       username: 'root',
-      password: '',
+      password: 'root',
       database: 'blog_db',
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
     }),
     UsersModule,
     AuthModule,
